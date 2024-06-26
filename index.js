73d4a7512ce1a93121e2bb4639ef5e8f24a7b67f
Ryan Maurer <ryan.maurer@mindera.com>
2024-06-26 13:54:51 -0400

    Commit after class on June 26

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const buttons = document.querySelectorAll("button");
 const inputEquationElement = document.querySelector("input#inputEquation");
 
+function runCalculator() {
+  console.log(
+    "I want to calculate:",
+    document.querySelector("#inputEquation").value
+  );
+  //Pseudocode for logic:
+  //1 - Split input into an array of inputs.
+  //'123*123'.split("*") => ['123','123'] => ['123','*','123']
+  //2 - Find and execute exponents
+  //3 - Find and execute multiplication and division
+  //4 - Find and execute addition and subtraction.
+
+  const inputString = document.querySelector("#inputEquation").value;
+  let inputArray = [];
+  const exponentArray = inputString.split("^");
+  exponentArray.forEach((exp, i) => {
+    inputArray.push(exp);
+    if (i < exponentArray.length - 1) {
+      inputArray.push("^");
+    }
+  });
+  console.log(exponentArray);
+  console.log(inputArray);
+}
+
 Array.from(buttons).forEach((btn) => {
   btn.addEventListener("click", () => {
     const currentString = inputEquationElement.value;
@@ -32,6 +57,25 @@ Array.from(buttons).forEach((btn) => {
         }
         newString = currentString + newCharacter;
         inputEquationElement.value = newString;
+        break;
+      case "constantContainer":
+        switch (btn.id) {
+          case "pi":
+            newCharacter = "pi";
+            break;
+          case "exp":
+            newCharacter = "e";
+            break;
+        }
+        newString = currentString + newCharacter;
+        if (btn.id === "clear") newString = "";
+        if (btn.id === "back") {
+          newString = currentString.substring(0, currentString.length - 1);
+        }
+        inputEquationElement.value = newString;
+        break;
+      case "equationHeader":
+        if (btn.id === "run") runCalculator();
         break;
     }
   });
