1ebf22d3e0a7751c237ec03701d5baac610cb080
Ryan Maurer <ryan.maurer@mindera.com>
2024-06-28 13:31:56 -0400

    interim commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const buttons = document.querySelectorAll("button");
 const inputEquationElement = document.querySelector("input#inputEquation");
 
+function splitAndBuildArray(characterToSplit, inputArray) {
+  let newArray = [];
+  //["12^2+3^3-1^0"]
+  inputArray.forEach((input) => {
+    if (input === characterToSplit) newArray.push(input); //error handling
+    else {
+      //"12^2+3^3-1^0"
+      const subArray = input.split(characterToSplit);
+      //subArray === ["12","2+3","3-1","0"]
+      subArray.forEach((inp, i) => {
+        newArray.push(inp);
+        if (i < subArray.length - 1) {
+          newArray.push(characterToSplit);
+        }
+      });
+    }
+  });
+
+  return newArray;
+}
+
 function runCalculator() {
   console.log(
     "I want to calculate:",
@@ -13,17 +34,45 @@ function runCalculator() {
   //3 - Find and execute multiplication and division
   //4 - Find and execute addition and subtraction.
 
-  const inputString = document.querySelector("#inputEquation").value;
-  let inputArray = [];
-  const exponentArray = inputString.split("^");
-  exponentArray.forEach((exp, i) => {
-    inputArray.push(exp);
-    if (i < exponentArray.length - 1) {
-      inputArray.push("^");
+  //part 1 - build array of inputs
+  const inputArray = [document.querySelector("#inputEquation").value];
+
+  let outputArray = [];
+
+  outputArray = splitAndBuildArray("^", inputArray);
+
+  console.log(inputArray);
+  console.log("Split by ^:", outputArray);
+  outputArray = splitAndBuildArray("*", outputArray);
+  console.log("Split by *:", outputArray);
+  outputArray = splitAndBuildArray("/", outputArray);
+  console.log("Split by /:", outputArray);
+  outputArray = splitAndBuildArray("+", outputArray);
+  console.log("Split by +:", outputArray);
+  outputArray = splitAndBuildArray("-", outputArray);
+  console.log("Split by -:", outputArray);
+
+  //Part 2 - Find and execute exponents
+  //1 - Tell JS to go through the array, one by one, and find '^'.
+  //2 - When it finds the ^, take the previous number and raise it to the next number
+  //3 - rebuild the array with the new, combined, value
+
+  outputArray.forEach((input, i) => {
+    if (input === "^" && i !== 0 && i < outputArray.length) {
+      //step 2 - get combined exponent
+      const lastNumber = outputArray[i - 1];
+      const nextNumber = outputArray[i + 1];
+      const combinedValue = Number(lastNumber) ** Number(nextNumber);
+      //step 3
+      let updatedArray = [];
+      for (let j = 0; j < outputArray.length; j++) {
+        if (j !== i && j !== i - 1 && j !== i + 1)
+          updatedArray.push(outputArray[j]);
+        else if (j === i) updatedArray.push(combinedValue);
+      }
+      console.log("Calculate exponents", updatedArray);
     }
   });
-  console.log(exponentArray);
-  console.log(inputArray);
 }
 
 Array.from(buttons).forEach((btn) => {
